0cffee88e573426b2b96f6851f49edc5850f0175
Georgy Schukin <georgy.schukin@gmail.com>
2017-11-09 12:47:21 +0700

    Update graph tests

--- graph/graph_algs_test.cpp
+++ graph/graph_algs_test.cpp
@@ -48,14 +48,63 @@ TEST_CASE( "Disconnected graph" ) {
     CHECK_FALSE( is_connected(g) );
 }
 
-TEST_CASE( "Path in null graph" ) {
+TEST_CASE( "Path in null graph doesn't exist" ) {
     Graph g;    
     CHECK_FALSE( path_exists(g, 0, 1) );
 }
 
-TEST_CASE( "Path in singleton graph" ) {
+TEST_CASE( "Path in singleton graph exists" ) {
     Graph g;    
     g.add_vertex(0);
     CHECK( path_exists(g, 0, 0) );
+}
+
+TEST_CASE( "Path for two vertices without edge" ) {
+    Graph g;    
+    g.add_vertex(0);
+    g.add_vertex(1);
+    CHECK_FALSE( path_exists(g, 0, 1) );
+}
+
+TEST_CASE( "Path for two vertices with edge" ) {
+    Graph g;    
+    g.add_edge(0, 1);
+    CHECK( path_exists(g, 0, 1) );
+}
+
+TEST_CASE( "Path for several vertices" ) {
+    Graph g;    
+    g.add_edge(0, 1);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(0, 3);
+    CHECK( path_exists(g, 0, 3) );
+    CHECK( path_exists(g, 1, 3) );
+}
+
+TEST_CASE( "Path in disconnected graph" ) {
+    Graph g;    
+    g.add_edge(0, 1);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(0, 3);
+    g.add_edge(4, 5);
+    g.add_edge(5, 6);
+    g.add_vertex(7);
+    CHECK( path_exists(g, 1, 3) );
+    CHECK_FALSE( path_exists(g, 0, 5) );
+    CHECK_FALSE( path_exists(g, 1, 7) );
+    CHECK_FALSE( path_exists(g, 5, 7) );
+}
+
+TEST_CASE( "Reverse path" ) {
+    Graph g;    
+    g.add_edge(0, 1);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(2, 4);
+    g.add_edge(3, 5);
+    CHECK( path_exists(g, 5, 0) );
+    CHECK( path_exists(g, 4, 1) );
 }
 
