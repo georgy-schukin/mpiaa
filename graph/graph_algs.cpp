8e3d9c58793f652090038f21f01687f0bee78963
Georgy Schukin <georgy.schukin@gmail.com>
2017-11-09 13:37:12 +0700

    Update graph tests

--- graph/graph_algs.cpp
+++ graph/graph_algs.cpp
@@ -1,13 +1,13 @@
 #include "graph_algs.h"
 
-bool is_connected(const Graph &g) {
+bool is_connected(const Graph &graph) {
     /// Should return true if graph is connected, false otherwise.
     /// Null graph (with no vertices) is connected.
     /// Singleton graph (with only one vertex) is connected.
     return false;
 }
 
-bool path_exists(const Graph &g, int start_vertex, int end_vertex) {
+bool path_exists(const Graph &graph, int start_vertex, int end_vertex) {
     /// Should return true if there is a path from start to end vertex.
     /// If start and end vertices are the same, path exists.
     return false;
--- graph/graph_algs.h
+++ graph/graph_algs.h
@@ -1,7 +1,7 @@
 #include "graph.h"
 
 /// Returns true if graph is connected, false otherwise.
-bool is_connected(const Graph &g);
+bool is_connected(const Graph &graph);
 
 /// Returns true if there is a path from start vertex to end vertex, false otherwise.
-bool path_exists(const Graph &g, int start_vertex, int end_vertex);
+bool path_exists(const Graph &graph, int start_vertex, int end_vertex);
