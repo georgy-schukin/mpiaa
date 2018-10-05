571f6a9f8c2517da192cd462f4ddc79a58559ccb
Georgy Schukin <georgy.schukin@gmail.com>
2018-10-05 09:04:28 +0700

    Update count sort example

--- count_sort/count_sort.cpp
+++ count_sort/count_sort.cpp
@@ -1,5 +1,7 @@
 #include "count_sort.h"
 
+// All elements of array are from [start, end] diapason.
+// Function must return sorted array.
 std::vector<int> count_sort(const std::vector<int> &array, int start, int end) {
     std::vector<int> result;
 
