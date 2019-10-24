6d682fd59ecfd6c8d518299d36f115e017ddd245
Georgy Schukin <georgy.schukin@gmail.com>
2019-10-25 00:07:03 +0700

    Update graph example tests

--- graph/path_test.cpp
+++ graph/path_test.cpp
@@ -4,18 +4,18 @@
 
 #include "path.h"
 
-TEST_CASE( "Path in an empty graph doesn't exist", "[path_exists]" ) {
+TEST_CASE( "Empty graph", "[path_exists]" ) {
     Graph g;    
     CHECK_FALSE( path_exists(g, 0, 1) );
 }
 
-TEST_CASE( "Path in singleton graph exists", "[path_exists]" ) {
+TEST_CASE( "Singleton graph", "[path_exists]" ) {
     Graph g;    
     g.add_vertex(0);
     CHECK( path_exists(g, 0, 0) );
 }
 
-TEST_CASE( "Path in singleton graph with a loop exists", "[path_exists]" ) {
+TEST_CASE( "Singleton graph with a loop", "[path_exists]" ) {
     Graph g;    
     g.add_vertex(0);
     g.add_arc(0, 0);
@@ -97,3 +97,35 @@ TEST_CASE( "Path in a graph with loops", "[path_exists]" ) {
     CHECK_FALSE( path_exists(g, 6, 7) );
 }
 
+TEST_CASE( "Graph with negative vertices", "[path_exists]" ) {
+    Graph g;    
+    g.add_arc(-1, -999);
+    g.add_arc(-1, -2);
+    g.add_arc(-2, 0);
+    g.add_arc(0, 999);
+    CHECK( path_exists(g, -1, -999) );
+    CHECK( path_exists(g, -1, 999) );
+    CHECK_FALSE( path_exists(g, -999, 999) );
+}
+
+TEST_CASE( "Graph with many loops", "[path_exists]" ) {
+    Graph g;    
+    g.add_arc(0, 1);
+    g.add_arc(1, 2);
+    g.add_arc(1, 3);
+    g.add_arc(1, 4);
+    g.add_arc(2, 0);
+    g.add_arc(3, 0);    
+    g.add_arc(2, 5);
+    g.add_arc(3, 5);
+    g.add_arc(4, 5);    
+    g.add_arc(5, 6); 
+    g.add_arc(6, 0);    
+    CHECK( path_exists(g, 0, 0) );    
+    CHECK( path_exists(g, 0, 6) );    
+    CHECK( path_exists(g, 1, 0) );    
+    CHECK( path_exists(g, 2, 0) );    
+    CHECK( path_exists(g, 5, 0) );        
+    CHECK( path_exists(g, 4, 1) );            
+    CHECK( path_exists(g, 2, 4) );            
+}
