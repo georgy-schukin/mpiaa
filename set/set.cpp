7190590b42b07ee8494f26fd46ac617d2213bd34
Georgy Schukin <georgy.schukin@gmail.com>
2020-10-01 09:40:25 +0700

    Update set example

--- set/set.cpp
+++ set/set.cpp
@@ -1,5 +1,6 @@
 #include "set.h"
 
+// A node of a binary tree.
 struct TreeNode {
 	int elem;
 	TreeNode *left = nullptr;
@@ -10,7 +11,8 @@ struct TreeNode {
 	}
 };
 
-int treeSize(TreeNode *node) {
+int treeSize(const TreeNode *node) {
+	// Get size of the tree recursively.
 	if (!node) {
 		return 0;
 	}
@@ -18,6 +20,7 @@ int treeSize(TreeNode *node) {
 }
 
 void treeDelete(TreeNode *node) {
+	// Delete the tree recursively.
 	if (!node) {
 		return;
 	}
@@ -30,12 +33,15 @@ Set::Set() {
 }
 
 Set::~Set() {    
+	// Destroy the tree.
     treeDelete(root);
 }
 
 void Set::insert(int elem) {
     // Insert the element into the set.
     // If the element is in the set already, do nothing.
+	// Inserting is done by creating new tree nodes, i.e. node = new TreeNode(elem),
+	// then adding them to the tree: root = node, or parent->left = node, or parent->right = node.
 
     // Your implementaton here.
 }
