f7f0abf086541111711a2889e92277927f8e2c7d
Georgy Schukin <georgy.schukin@gmail.com>
2020-10-21 18:52:20 +0700

    Update closest pair example

--- closest_pair/closest_pair.cpp
+++ closest_pair/closest_pair.cpp
@@ -5,12 +5,16 @@
 using namespace std;
 
 std::pair<Point, Point> closest_pair(const std::vector<Point> &points) {
-	if (points.size() <= 1) {
+	// Return the closest pair of points from given points.
+	// Order of points in the result doesn't matter.
+	// If there are fewer than 2 points, throw an exception.
+
+	if (points.size() < 2) {
 		throw invalid_argument("Not enough points");
 	}	
 
 	// Your implementation here.
 	
-	auto closest = std::make_pair(Point(), Point());
-	return closest;
+	auto result = std::make_pair(Point(), Point());	
+	return result;
 }
