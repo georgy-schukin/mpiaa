8f21c56360b3135e6a2ce2bd5f5e566425850620
Georgy Schukin <georgy.schukin@gmail.com>
2020-11-02 17:26:13 +0700

    Change sprintf in sha256 to snprintf in bruteforce example

--- bruteforce/sha256.cpp
+++ bruteforce/sha256.cpp
@@ -127,6 +127,6 @@ std::string sha256(std::string input)
     char buf[2*SHA256::DIGEST_SIZE+1];
     buf[2*SHA256::DIGEST_SIZE] = 0;
     for (int i = 0; i < SHA256::DIGEST_SIZE; i++)
-        sprintf(buf+i*2, "%02x", digest[i]);
+        snprintf(buf+i*2, 2*SHA256::DIGEST_SIZE + 1 - i*2, "%02x", digest[i]);
     return std::string(buf);
 }
